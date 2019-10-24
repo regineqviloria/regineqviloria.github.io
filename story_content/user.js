8d637303b999e1cdc6cfb3c3364abbc04c5b8906
rochevil <rochevil@users.noreply.github.com>
2019-10-24 22:56:15 +0800

    test

--- story_content/user.js
+++ story_content/user.js
@@ -2,13 +2,13 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5wq205ikH7C":
+      case "6g94SCYIyBV":
         Script1();
         break;
-      case "69wxY4NUdJB":
+      case "6cWxJ8z96Qp":
         Script2();
         break;
-      case "6Gh36MVZXdD":
+      case "5zQ1aiNhHWD":
         Script3();
         break;
   }
