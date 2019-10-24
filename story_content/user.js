774cad44aa64d944bb3ce3565e6a58bcaf6a15f8
rochevil <rochevil@users.noreply.github.com>
2019-10-24 17:53:59 +0800

    NEW TRAINING

--- story_content/user.js
+++ story_content/user.js
@@ -1,1 +1,18 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68BUazQ2kFh":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("problemsforcust");
+var numChars= Answer.length;
+player.SetVar("answerlength1",numChars);
+}
+
