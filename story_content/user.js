7289e57949f22db944b3db709b1e0ad1ea9374e7
rochevil <rochevil@users.noreply.github.com>
2019-11-13 16:29:30 +0800

    test

--- story_content/user.js
+++ story_content/user.js
@@ -2,14 +2,50 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6KS4jlZg76Z":
+      case "66gwsERgyzA":
         Script1();
         break;
-      case "6bB2yWiSLiM":
+      case "6Pu3SQaF5LU":
         Script2();
         break;
-      case "66zqa2zj8bU":
+      case "6ArtTcGIqrU":
         Script3();
+        break;
+      case "6MXHf0JtuV2":
+        Script4();
+        break;
+      case "6e3HmxFIa1o":
+        Script5();
+        break;
+      case "6SAZagNrKk8":
+        Script6();
+        break;
+      case "6XKAyE3e0Jg":
+        Script7();
+        break;
+      case "6iJUQ30czf5":
+        Script8();
+        break;
+      case "6LIRNaShJB7":
+        Script9();
+        break;
+      case "5wXCDjDtXyZ":
+        Script10();
+        break;
+      case "698cm8Mcw3S":
+        Script11();
+        break;
+      case "6YecmSOwW17":
+        Script12();
+        break;
+      case "6BubzKJqJEQ":
+        Script13();
+        break;
+      case "6SvdyYX8Lll":
+        Script14();
+        break;
+      case "6quZgyMkQUR":
+        Script15();
         break;
   }
 }
@@ -36,5 +72,101 @@ function Script3()
 var Answer= player.GetVar("problemsforelectro");
 var numChars= Answer.length;
 player.SetVar("answerlength1",numChars);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("applystep1q1");
+var numChars= Answer.length;
+player.SetVar("answerlength3",numChars);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("applystep1q2");
+var numChars= Answer.length;
+player.SetVar("answerlength3",numChars);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("designanswers1");
+var numChars= Answer.length;
+player.SetVar("answerlength2",numChars);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("designanswers2");
+var numChars= Answer.length;
+player.SetVar("answerlength2",numChars);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("compareq1");
+var numChars= Answer.length;
+player.SetVar("answerlength4",numChars);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("compareq2");
+var numChars= Answer.length;
+player.SetVar("answerlength4",numChars);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("compareq3");
+var numChars= Answer.length;
+player.SetVar("answerlength4",numChars);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("compareq4");
+var numChars= Answer.length;
+player.SetVar("answerlength4",numChars);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("compareq5");
+var numChars= Answer.length;
+player.SetVar("answerlength4",numChars);
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("compareq6");
+var numChars= Answer.length;
+player.SetVar("answerlength4",numChars);
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("implementq1");
+var numChars= Answer.length;
+player.SetVar("answerlength5",numChars);
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+var Answer= player.GetVar("implementq2");
+var numChars= Answer.length;
+player.SetVar("answerlength5",numChars);
 }
 
